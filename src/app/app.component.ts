7454fd93a956cae00e72b6ec77a483c4d9107a1a
sountastic <paulina.gr@o2.pl>
2021-01-06 13:38:30 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+
+export interface Products {
+  product: string;
+  qty: number;
+  price: number;
+  total: number;
+}
+
+const ELEMENT_DATA: Products[] = [
+  {product: 'Pizza', qty: 3, price: 1.0079, total: 3.0095},
+  {product: 'Pizza', qty: 3, price: 1.0079, total: 3.333},
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+  displayedColumns: string[] = ['product', 'qty', 'price', 'total'];
+  columnsToDisplay: string[] = this.displayedColumns.slice();
+  data: Products[] = ELEMENT_DATA;
+
   title = 'inv-app';
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
